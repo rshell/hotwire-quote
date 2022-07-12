0ac8509b8e1c96687d7564f489ed66c3468a0b69
rshell <rshell@edge-ka.com>
2022-07-12 12:05:54 +0100

    FLASH

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,12 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
   belongs_to :company
 
+  delegate :name ,to: :company, prefix: true, allow_nil: true
+
+  def path
+    [company_name,name].join("/")
+  end
+
   #after_create_commit -> { broadcast_prepend_later_to "quotes" }
   #after_update_commit -> { broadcast_replace_later_to "quotes" }
   #after_destroy_commit -> { broadcast_remove_to "quotes" }
