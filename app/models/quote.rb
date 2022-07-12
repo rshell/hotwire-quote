7b958943ce74b793d56787c47825e68560d403e4
rshell <rshell@edge-ka.com>
2022-07-12 14:16:44 +0100

    -only-child switch in css

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,7 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
   belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
 
   delegate :name ,to: :company, prefix: true, allow_nil: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_102259) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_12_130433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_102259) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "line_item_dates", force: :cascade do |t|
+    t.bigint "quote_id", null: false
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quote_id"], name: "index_line_item_dates_on_quote_id"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "line_item_date_id", null: false
+    t.string "name"
+    t.text "description"
+    t.integer "quantity"
+    t.decimal "unit_price", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["line_item_date_id"], name: "index_line_items_on_line_item_date_id"
   end
 
   create_table "quotes", force: :cascade do |t|
@@ -42,6 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_102259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "line_item_dates", "quotes"
+  add_foreign_key "line_items", "line_item_dates"
   add_foreign_key "quotes", "companies"
   add_foreign_key "users", "companies"
 end
